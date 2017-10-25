19263f97ebbb8ae96a9e8cfbd7653b3fc864e74e
Thomas Desmond <thomasdes533@gmail.com>
2017-10-24 18:34:00 -0700

    Initial Commit

---
+++ Week9PrismExampleApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using Week9PrismExampleApp.Views;
+
+namespace Week9PrismExampleApp
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("MainPage");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
+
---
+++ Week9PrismExampleApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Week9PrismExampleApp.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainPageViewModel()
+        {
+            Title = "Xamarin Forms Application + Prism";
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+    }
+}
+
