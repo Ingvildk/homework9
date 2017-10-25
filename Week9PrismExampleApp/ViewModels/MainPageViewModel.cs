ec6b2db03b1fbe26140c208c7aeefdc7d7e63dad
Thomas Desmond <thomasdes533@gmail.com>
2017-10-24 18:44:57 -0700

    Added ListView of names to MainPage

--- Week9PrismExampleApp/ViewModels/MainPageViewModel.cs
+++ Week9PrismExampleApp/ViewModels/MainPageViewModel.cs
@@ -4,6 +4,7 @@ using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections.ObjectModel;
 
 namespace Week9PrismExampleApp.ViewModels
 {
@@ -14,6 +15,13 @@ namespace Week9PrismExampleApp.ViewModels
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
+        }
+
+        private ObservableCollection<string> _collectionOfNames = new ObservableCollection<string>();
+        public ObservableCollection<string> CollectionOfNames
+        {
+            get { return _collectionOfNames; }
+            set { SetProperty(ref _collectionOfNames, value); }
         }
 
         public MainPageViewModel()
@@ -28,13 +36,23 @@ namespace Week9PrismExampleApp.ViewModels
 
         public void OnNavigatedTo(NavigationParameters parameters)
         {
-
+            PopulateListView();
         }
 
         public void OnNavigatingTo(NavigationParameters parameters)
         {
 
         }
+
+		private void PopulateListView()
+		{
+            CollectionOfNames.Add("Thomas");
+			CollectionOfNames.Add("Charlie");
+			CollectionOfNames.Add("James");
+			CollectionOfNames.Add("Jennifer");
+			CollectionOfNames.Add("Sharon");
+			CollectionOfNames.Add("Aaron");
+		}
     }
 }
 
